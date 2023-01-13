7da86a755a967633c930cfc1e06b7598a9c5b8e0
dannyLiu <h152074716@gmail.com>
2023-01-14 01:07:57 +0800

    347 將 HighlightDirective 轉換成 Standalone Directive

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,10 +1,10 @@
-import { SharedModule } from './../../shared/shared.module';
+import { HighlightDirective } from './../../shared/highlight.directive';
 import { Component } from '@angular/core';
 import { AnalyticsService } from 'src/app/shared/analytics.service';
 
 @Component({
   standalone: true,
-  imports: [SharedModule],
+  imports: [HighlightDirective],
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css'],
